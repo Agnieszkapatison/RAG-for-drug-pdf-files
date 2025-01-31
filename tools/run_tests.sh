e979733e09c4c7763c3b4ee814f2d7684ea76d71
AgnieszkaWylupek <agnieszka.chrastek@wp.pl>
2025-01-31 13:22:56 +0100

    modyfied tools

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+
+# Ustawienie katalogu z kodem źródłowym jako ścieżki dla Pythona
 export PYTHONPATH=src
 
+# Uruchomienie testów z mierzeniem pokrycia kodu
+coverage run -m pytest tests/
+
+# Generowanie raportów pokrycia
 coverage xml -o "reports/coverage.xml"
 coverage html -d "reports/coverage_html"
 coverage report
